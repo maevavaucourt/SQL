776512a508d88078e56c1053c3bd3e877cfde00f
maevavaucourt <79145915+maevavaucourt@users.noreply.github.com>
2021-02-19 13:25:35 +0100

    Delete req5.sql

--- req5.sql
+++
@@ -1,6 +0,0 @@
-#05 Quel est le titre original du film 'Les dents de la mer' ? 
-SELECT title_basics.originalTitle
-FROM title_basics, title_akas
-WHERE title_akas.title = 'Les dents de la mer'
-AND titleType = 'movie'
-AND title_akas.titleId = title_basics.tconst;
