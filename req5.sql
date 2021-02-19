39fa667e3972f7e390ba3e848df3f147998f0ffb
maevavaucourt <79145915+maevavaucourt@users.noreply.github.com>
2021-02-19 10:40:16 +0100

    Update req5.sql

--- req5.sql
+++ req5.sql
@@ -1,3 +1,6 @@
-SELECT MIN ( startYear )
-FROM title_basics
-WHERE primaryTitle = "Superman" AND  titleType = "movie";
+#05 Quel est le titre original du film 'Les dents de la mer' ? 
+SELECT title_basics.originalTitle
+FROM title_basics, title_akas
+WHERE title_akas.title = 'Les dents de la mer'
+AND titleType = 'movie'
+AND title_akas.titleId = title_basics.tconst;
