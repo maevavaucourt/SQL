6f9c21d0f1776f72af196ed9e6962df4fc3f6684
maevavaucourt <79145915+maevavaucourt@users.noreply.github.com>
2021-02-24 14:26:09 +0100

    Delete req5.sql

--- req5.sql
+++
@@ -1,5 +0,0 @@
-SELECT title_basics.originalTitle
-FROM title_basics, title_akas
-WHERE title_akas.title = 'Les dents de la mer'
-AND titleType = 'movie'
-AND title_akas.titleId = title_basics.tconst;
