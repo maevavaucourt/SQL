d3fad9ab68f633721cdf205485f66123b5670934
maevavaucourt <79145915+maevavaucourt@users.noreply.github.com>
2021-03-26 14:40:02 +0100

    Delete req21.sql

--- req21.sql
+++
@@ -1,7 +0,0 @@
-SELECT primaryTitle
-FROM title_basics
-JOIN title_episode ON title_episode.tconst = title_basics.tconst
-WHERE parentTconst IN(SELECT title_basics.tconst
-FROM title_basics
-WHERE primaryTitle = "Game of Thrones"
-AND titleType = "tvSeries")
