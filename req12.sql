9d1b9eecd0d12df5b79d90b5de3c72fbf7580457
maevavaucourt <79145915+maevavaucourt@users.noreply.github.com>
2021-03-26 14:37:20 +0100

    Delete req12.sql

--- req12.sql
+++
@@ -1,6 +0,0 @@
-SELECT originalTitle
-FROM title_basics
-INNER JOIN title_ratings ON title_basics.tconst = title_ratings.tconst
-WHERE genres LIKE "%Romance%" AND genres LIKE "%Comedy%"
-ORDER BY averageRating DESC
-LIMIT 5;
