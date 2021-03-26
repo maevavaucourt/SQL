b71bb7a2ee82ae9e716c86e6525fa55ed7a96a59
maevavaucourt <79145915+maevavaucourt@users.noreply.github.com>
2021-03-26 14:39:22 +0100

    Delete req19.sql

--- req19.sql
+++
@@ -1,5 +0,0 @@
-SELECT originalTitle, primaryName
-FROM title_principals, name_basics, title_basics
-WHERE titleType = "movie" AND characters LIKE "%James Bond%" 
-AND title_principals.nconst = name_basics.nconst
-AND title_principals.tconst = title_basics.tconst;
