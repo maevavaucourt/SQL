835ad6ed07bb795953f8329a281af26b55986a9f
maevavaucourt <79145915+maevavaucourt@users.noreply.github.com>
2021-03-26 14:41:08 +0100

    Delete req7.sql

--- req7.sql
+++
@@ -1,7 +0,0 @@
-SELECT originalTitle
-FROM title_basics,title_principals, name_basics
-WHERE titleType = "movie"
-AND name_basics.primaryName = "Olivier Nakache"
-AND name_basics.nconst = title_principals.nconst
-AND title_principals.tconst = title_basics.tconst;
- 
