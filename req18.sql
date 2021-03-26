7e2d9cd1c43d5fa9e8cddc123571cab00253f4dd
maevavaucourt <79145915+maevavaucourt@users.noreply.github.com>
2021-03-26 14:39:09 +0100

    Delete req18.sql

--- req18.sql
+++
@@ -1,6 +0,0 @@
-SELECT originalTitle
-FROM title_basics
-JOIN name_titles ON knownForTitles = tconst
-JOIN name_basics ON name_basics.nconst = name_titles.nconst
-WHERE primaryName = "Sean Connery"
-AND titleType = "movie"
