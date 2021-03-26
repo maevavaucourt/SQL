938286a7d97b610f8f394146e8867fb687ba9eed
maevavaucourt <79145915+maevavaucourt@users.noreply.github.com>
2021-03-26 14:37:06 +0100

    Delete req11.sql

--- req11.sql
+++
@@ -1,4 +0,0 @@
-SELECT originalTitle
-FROM title_basics
-INNER JOIN title_ratings ON title_basics.tconst = title_ratings.tconst
-WHERE averageRating > 9 AND numVotes > 10000 AND titleType = "movie";
